25f3af82c79839222acdc21c61f4377db9ae1528
CunningFox146 <makar.papca@gmail.com>
2020-10-26 14:24:33 +0300

    Added Set class
    Overrided - operator

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -6,10 +6,59 @@ using System.Threading.Tasks;
 
 namespace lab4
 {
+    class Set
+    {
+        private List<string> mData;
+
+        public Set()
+        {
+            mData = new List<string>();
+        }
+
+        public Set(string[] items)
+        {
+            mData = new List<string>(items);
+        }
+
+        public string this[int i]
+        {
+            get
+            {
+                return mData[i];
+            }
+            set
+            {
+                mData[i] = value;
+            }
+        }
+
+        public static Set operator -(Set a, string b)
+        {
+            Set new_set = (Set)a.MemberwiseClone();
+
+            new_set.mData.Remove(b);
+
+            return new_set;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            foreach (string item in mData)
+            {
+                str.Append($"\t{item}");
+            }
+            return str.ToString();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var Set1 = new Set(new string[] { "1", "2", "a" });
+
+            Console.WriteLine(Set1 - "a");
         }
     }
 }
